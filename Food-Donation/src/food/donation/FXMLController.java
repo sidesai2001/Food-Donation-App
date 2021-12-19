f1a378b24f2172de3b9d0c06bf3a968a9c34336c
sidesai2001 <sidesai2001@gmail.com>
2021-12-20 02:15:57 +0530

    changes

--- Food-Donation/src/food/donation/FXMLController.java
+++ Food-Donation/src/food/donation/FXMLController.java
@@ -7,25 +7,18 @@ package food.donation;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author Siddhant Desai
  */
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        
-    }
-    
+public class FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
