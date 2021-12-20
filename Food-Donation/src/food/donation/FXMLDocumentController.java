c33d6ed3a5d90d81a6ed8b17838076f887ffb152
sidesai2001 <sidesai2001@gmail.com>
2021-12-20 21:05:15 +0530

    changes

--- Food-Donation/src/food/donation/FXMLDocumentController.java
+++ Food-Donation/src/food/donation/FXMLDocumentController.java
@@ -5,12 +5,17 @@
  */
 package food.donation;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
@@ -18,12 +23,17 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        
+    private void handleButtonAction(ActionEvent event) throws IOException {
+       root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+       stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+       scene = new Scene(root);
+       stage.setScene(scene);
+       stage.show();
     }
     
     @Override
--- Food-Donation/src/food/donation/FoodDonation.java
+++ Food-Donation/src/food/donation/FoodDonation.java
@@ -21,9 +21,7 @@ public class FoodDonation extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
