281251b8c160aa4849f732af73be305d87c247a1
starix2436 <75304097+starix2436@users.noreply.github.com>
2021-12-26 00:55:54 +0530

    changes

---
+++ Food-Donation/src/food/donation/mysqlconnect.java
@@ -0,0 +1,23 @@
+
+package food.donation;
+
+import java.sql.*;
+
+public class mysqlconnect 
+{
+   public Connection conn;
+   
+    public Connection ConnectDb()
+    {
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/food_doantion_app","root","");
+        } 
+        catch (ClassNotFoundException | SQLException e) 
+        {
+        }  
+        return conn;
+    }
+}
+
